ef39c3fd7f6cfff6df66f94544fa3a67daa2cfb8
naSim087 <nasimsheikh688@gmail.com>
2024-06-11 16:55:47 +0530

    added the vituals

--- src/repository/tweet-repository.js
+++ src/repository/tweet-repository.js
@@ -34,6 +34,9 @@ async get(id){
 async getWithComment(id){
   try {
     const tweet=await Tweet.findById(id).populate({path:'comments'});
+    // Tweet.findById(id).populate({path:'comments'}).lean can be used when we don't want the 
+    // object to me moongose using the lean we will get the javascript object
+    
     return tweet;
   } catch (error) {
     console.log(`something went wrong at the repolayer`);
@@ -50,6 +53,15 @@ async destroy(id){
   }
 }
 
+// writing the query to paginate the going response
+async getAll(offset,limit){
+  try {
+    const tweet= await Tweet.find().skip(offset).limit(limit);
+    return tweet;
+  } catch (error) {
+    throw error;
+  }
+}
 
 
 
